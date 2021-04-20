c805d52a2d80d256eb55dcd73ff3d131b60780f6
dnesmith <git@ajdn.org>
2021-04-19 21:30:12 -0400

    adding create/update/get for campaigns/line-items

--- lib/criteo-api-retail.d.ts
+++ lib/criteo-api-retail.d.ts
@@ -16,7 +16,7 @@ interface PageInfo {
 interface AccountsResponse {
     data: [
         {
-            id:string,
+            id: string,
             type: string,
             attributes: {
                 name: string
@@ -34,57 +34,102 @@ interface AccountsResponse {
     metadata: MetaData
 }
 
+interface CampaignAttributes {
+    accountId: string,
+    promotedBrandIds: string[],
+    budgetSpent: number,
+    budgetRemaining: number,
+    status: "active" | "inactive",
+    createdAt: string,
+    updatedAt: string,
+    type?: "auction" | "preferred",
+    drawableBalanceIds: string[],
+    clickAttributionWindow: "7D" | "14D" | "30D",
+    viewAttributionWindow: "none" | "1D" | "7D" | "14D" | "30D",
+    name: string,
+    budget: number | null
+}
+
+type RequireOnly<T, K extends keyof T> = Required<Pick<T, K>> & Partial<Omit<T, K>>;
+
+interface CreateCampaign {
+    data: {
+        type: "RetailMediaCampaign",
+        attributes: RequireOnly<CampaignAttributes, "name">
+    }
+}
+
+interface UpdateCampaign {
+    data: {
+        id: string,
+        type: "RetailMediaCampaign",
+        attributes: RequireOnly<CampaignAttributes, "name" | "budget" | "clickAttributionWindow" | "viewAttributionWindow">
+    }
+}
+
+interface CampaignResponse {
+    data: {
+        id: string,
+        type: "RetailMediaCampaign",
+        attributes: CampaignAttributes
+    }
+}
+
 interface CampaignsResponse {
-    data: [
-        {
-            id: string,
-            type: string,
-            attributes: {
-                accountId: string,
-                promotedBrandIds: string[],
-                budgetSpent: number,
-                budgetRemaining: number,
-                status: "active" | "inactive",
-                createdAt: string,
-                updatedAt: string,
-                type?: "auction" | "preferred",
-                drawableBalanceIds: string[],
-                clickAttributionWindow: "7D" | "14D" | "30D",
-                viewAttributionWindow: "none" | "1D" | "7D" | "14D" | "30D",
-                name: string,
-                budget: number | null
-            }
-        }
-    ],
+    data: [{
+        id: string,
+        type: "RetailMediaCampaign",
+        attributes: CampaignAttributes
+    }],
+    metadata: MetaData
+}
+
+interface LineItemAttributes {
+    campaignId: string,
+    budgetSpent: number,
+    budgetRemaining: number | null,
+    createdAt: string,
+    updatedAt: string,
+    targetRetailerId: string,
+    status: "active" | "paused" | "scheduled" | "ended" | "budgetHit" | "noFunds" | "draft" | "archived",
+    targetBid: number,
+    isAutoDailyPacing: boolean,
+    name: string,
+    startDate: string,
+    endDate?: string | null,
+    maxBid: number | null,
+    budget: number | null,
+    monthlyPacing: number | null,
+    dailyPacing: number | null,
+    bidStrategy: "conversion" | "revenue" | "clicks"
+}
+
+interface CreateLineItem {
+    type: "RetailMediaLineItem",
+    attributes: RequireOnly<LineItemAttributes, "name" | "targetRetailerId" | "startDate">
+}
+
+interface UpdateLineItem {
+    id: string,
+    type: "RetailMediaLineItem",
+    attributes: RequireOnly<LineItemAttributes, "name" | "startDate" | "endDate" | "budget" | "monthlyPacing" | "dailyPacing" | "isAutoDailyPacing" | "bidStrategy" | "targetBid" | "maxBid" | "status">
+}
+
+interface LineItemResponse {
+    data: {
+        id: string,
+        type: "RetailMediaLineItem",
+        attributes: LineItemAttributes
+    },
     metadata: MetaData
 }
 
 interface LineItemsResponse {
-    data: [
-        {
-            id: string,
-            type: string,
-            attributes: {
-                campaignId: string,
-                budgetSpent: number,
-                budgetRemaining: number | null,
-                createdAt: string,
-                updatedAt: string,
-                targetRetailerId: string,
-                status: "active" | "paused" | "scheduled" | "ended" | "budgetHit" | "noFunds" | "draft" | "archived",
-                targetBid: number,
-                isAutoDailyPacing: boolean,
-                name: string,
-                startDate: string,
-                endDate?: string | null,
-                maxBid: number | null,
-                budget: number | null,
-                monthlyPacing: number | null,
-                dailyPacing: number | null,
-                bidStrategy: "conversion" | "revenue" | "clicks"
-            }
-        }
-    ],
+    data: [{
+        id: string,
+        type: "RetailMediaLineItem",
+        attributes: LineItemAttributes
+    }],
     metadata: MetaData
 }
 
@@ -117,11 +162,29 @@ interface ReportStatusResponse {
 
 declare class Criteo_API {
     constructor(key: string, secret: string);
-    getAccounts(callback?:(err: any, res: any) => void): Promise<AccountsResponse | undefined>;
-    getCampaignsByAccountId(accountId:string, pageInfo?:PageInfo, callback?:(err: any, res: any) => void): Promise<CampaignsResponse | undefined>;
-    getLineItemsByCampaignId(campaignId:string, pageInfo?:PageInfo, callback?:(err: any, res: any) => void): Promise<LineItemsResponse | undefined>;
-    getReport(reportType:"campaigns" | "line-items", query:ReportQuery, callback?:(err: any, res: any) => void): Promise<ReportStatusResponse | undefined>;
-    getReportStatus(reportId:string, callback?:(err: any, res: any) => void): Promise<ReportStatusResponse | undefined>;
-    getReportOutput(reportId:string, filepath?:string, callback?:(err: any, res: any) => void): Promise<ReportStatusResponse | undefined>;
+
+    getAccounts(callback?: (err: any, res: any) => void): Promise<AccountsResponse | undefined>;
+
+    getCampaignsByAccountId(accountId: string, pageInfo?: PageInfo, callback?: (err: any, res: any) => void): Promise<CampaignsResponse | undefined>;
+
+    createCampaignByAccountId(accountId: string, campaignData: CreateCampaign, callback?: (err: any, res: any) => void): Promise<CampaignResponse | undefined>;
+
+    getCampaignById(campaignId: string, callback?: (err: any, res: any) => void): Promise<CampaignResponse | undefined>;
+
+    updateCampaignById(campaignId: string, campaignData: UpdateCampaign, callback?: (err: any, res: any) => void): Promise<CampaignResponse | undefined>;
+
+    getLineItemsByCampaignId(campaignId: string, pageInfo?: PageInfo, callback?: (err: any, res: any) => void): Promise<LineItemsResponse | undefined>;
+
+    createLineItemByCampaignId(accountId: string, lineItemData: CreateLineItem, callback?: (err: any, res: any) => void): Promise<LineItemResponse | undefined>;
+
+    getLineItemById(lineItemId: string, callback?: (err: any, res: any) => void): Promise<LineItemResponse | undefined>;
+
+    updateLineItemById(lineItemId: string, lineItemData: UpdateLineItem, callback?: (err: any, res: any) => void): Promise<LineItemResponse | undefined>;
+
+    getReport(reportType: "campaigns" | "line-items", query: ReportQuery, callback?: (err: any, res: any) => void): Promise<ReportStatusResponse | undefined>;
+
+    getReportStatus(reportId: string, callback?: (err: any, res: any) => void): Promise<ReportStatusResponse | undefined>;
+
+    getReportOutput(reportId: string, filepath?: string, callback?: (err: any, res: any) => void): Promise<ReportStatusResponse | undefined>;
 }
 
